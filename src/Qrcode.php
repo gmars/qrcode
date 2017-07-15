eee1d3c30c0e203747d881f9a3f859e1a0302ab4
weiyongqiang <weiyongqiang@weiyongqiang.com>
2017-07-15 22:46:26 +0800

    初次提交

---
+++ src/Qrcode.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mars
+ * Time: 2017/7/15 下午10:33
+ * Author: weiyongqiang <hayixia606@163.com>
+ * Website: www.weiyongqiang.com
+ */
+
+namespace gmars\qrcode;
+
+
+use gmars\qrcode\driver\PngCode;
+use yii\base\Component;
+
+class Qrcode extends Component
+{
+    public function test()
+    {
+        $pngObj = new PngCode();
+        return $pngObj->getPng();
+    }
+
+}
